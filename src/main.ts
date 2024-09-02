77bee501f66cfb29c808f35a5ecef18a7fc9a8bd
Duc <thienductranhuu2784@gmail.com>
2024-09-02 14:02:40 -0400

    view engine ejs, server side rendering

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'public'));     //useStaticAssets allows a page to access js, css, img in public folder when being rendered
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));    //storing view engine in views folder
+  app.setViewEngine('ejs'); 
+
   await app.listen(5000);
 }
 bootstrap();
