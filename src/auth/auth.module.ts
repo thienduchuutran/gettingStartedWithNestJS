33c1677322fb41280a9fbd5d0655113cba1f12cb
Duc <thienductranhuu2784@gmail.com>
2024-09-03 22:14:13 -0400

    solution to deal with dependencies injection errors

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UsersService } from 'src/users/users.service';
 import { UsersModule } from 'src/users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './passport/local.strategy';
 
 @Module({
-  providers: [AuthService],  //we need UsersService since it's in another folder, and this auth is another module
-  imports: [UsersModule]})  //if we import a whole UsersModule, we can use whatever in UsersModule
+  imports: [UsersModule, PassportModule],
+  providers: [AuthService, LocalStrategy],
+    //we need UsersService since it's in another folder, and this auth is another module
+    //if we import a whole UsersModule, we can use whatever in UsersModule
 export class AuthModule {}
