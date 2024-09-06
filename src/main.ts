8326c5ac8a8950c1ef8bf1951dd1ff231a02d2f8
Duc <thienductranhuu2784@gmail.com>
2024-09-05 21:23:47 -0400

    config cors error

--- src/main.ts
+++ src/main.ts
@@ -5,21 +5,27 @@ import { join } from 'path';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
-require('dotenv').config()
-
+require('dotenv').config();
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  const configService = app.get(ConfigService);     //since main.ts file ain't have constructor, we gotta add this to use .env file
+  const configService = app.get(ConfigService); //since main.ts file ain't have constructor, we gotta add this to use .env file
 
-  const reflector = app.get( Reflector)
-  app.useGlobalGuards( new JwtAuthGuard( reflector))
+  const reflector = app.get(Reflector);
+  // app.useGlobalGuards( new JwtAuthGuard( reflector))
 
-  app.useStaticAssets(join(__dirname, '..', 'public'));     //useStaticAssets allows a page to access js, css, img in public folder when being rendered
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));    //storing view engine in views folder
-  app.setViewEngine('ejs'); 
+  app.useStaticAssets(join(__dirname, '..', 'public')); //useStaticAssets allows a page to access js, css, img in public folder when being rendered
+  app.setBaseViewsDir(join(__dirname, '..', 'views')); //storing view engine in views folder
+  app.setViewEngine('ejs');
 
   app.useGlobalPipes(new ValidationPipe());
+
+  //config CORS
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+  });
   await app.listen(configService.get<string>('PORT'));
 }
 bootstrap();
