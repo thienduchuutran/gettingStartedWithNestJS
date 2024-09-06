c14d2cd2faac88b206babab67ea24b89cae5a226
Duc <thienductranhuu2784@gmail.com>
2024-09-06 12:51:08 -0400

    delete a company

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -5,25 +5,25 @@ import { Company, CompanyDocument } from './schema/company.schema';
 import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { IUser } from 'src/users/users.interface';
+import { use } from 'passport';
 
 @Injectable()
 export class CompaniesService {
   constructor(
-    @InjectModel(Company.name)  //gotta inject the company schema here to interact with company schema in db
-    private companyModal: SoftDeleteModel<CompanyDocument>
-  ){
-
-  }
-  async create(createCompanyDto: CreateCompanyDto, user: IUser) { //getting info of the user who creates the company
+    @InjectModel(Company.name) //gotta inject the company schema here to interact with company schema in db
+    private companyModal: SoftDeleteModel<CompanyDocument>,
+  ) {}
+  async create(createCompanyDto: CreateCompanyDto, user: IUser) {
+    //getting info of the user who creates the company
     return await this.companyModal.create({
       // name: createCompanyDto.name,
       // address: createCompanyDto.address,
       // description: createCompanyDto.description
-    ...createCompanyDto,
-    createdBy: {
-      _id: user._id,
-      email: user.email
-    }
+      ...createCompanyDto,
+      createdBy: {
+        _id: user._id,
+        email: user.email,
+      },
     });
   }
 
@@ -35,11 +35,36 @@ export class CompaniesService {
     return `This action returns a #${id} company`;
   }
 
-  update(id: number, updateCompanyDto: UpdateCompanyDto) {
-    return `This action updates a #${id} company`;
+  async update(id: string, updateCompanyDto: UpdateCompanyDto, user: IUser) {
+    return await this.companyModal.updateOne(
+      {
+        _id: id,
+      }, //this is finding the right company using company id
+      {
+        ...updateCompanyDto,
+        updatedBy: {
+          _id: user._id,
+          email: user.email,
+        },
+      },
+    );
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} company`;
+  async remove(id: string, user: IUser) {
+    //since softDelete ain't support deletedBy, we gotta use updateOne here
+    await this.companyModal.updateOne(
+      {
+        _id: id,
+      },
+      {
+        deletedBy: {
+          _id: user._id,
+          email: user.email,
+        },
+      },
+    );
+    return this.companyModal.softDelete({
+      _id: id,
+    });
   }
 }
