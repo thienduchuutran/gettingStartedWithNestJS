131d852ff152df8af6bbcd7a0f57e1bca4fbae6c
Duc <thienductranhuu2784@gmail.com>
2024-09-12 18:16:07 -0400

    config destination to upload files

--- src/files/multer.config.ts
+++ src/files/multer.config.ts
@@ -1,11 +1,61 @@
 import { Injectable } from "@nestjs/common";
 import { MulterModuleOptions, MulterOptionsFactory } from "@nestjs/platform-express";
+import fs from 'fs'
+import { diskStorage } from "multer";
+import path, { join } from "path";
 
 @Injectable()
 export class MulterConfigService implements MulterOptionsFactory {
-  createMulterOptions(): MulterModuleOptions {
-    return {
-      dest: './upload',
+    //this function returns the link to our root file
+    getRootPath = () => {
+        return process.cwd();
     };
-  }
+
+    //this check is the file ain't exist, it's gonna create for us 
+    ensureExists(targetDirectory: string) {
+        fs.mkdir(targetDirectory, { recursive: true }, (error) => {
+            if (!error) {
+                console.log('Directory successfully created, or it already exists.');
+                return;
+            }
+            switch (error.code) {
+                case 'EEXIST':
+                    // Error:
+                    // Requested location already exists, but it's not a directory.
+                    break;
+                case 'ENOTDIR':
+                    // Error:
+                    // The parent hierarchy contains a file with the same name as the dir
+                    // you're trying to create.
+                    break;
+                default:
+                    // Some other error like permission denied.
+                    console.error(error);
+                    break;
+            }
+        });
+    }
+
+    createMulterOptions(): MulterModuleOptions {
+        return {
+            //this storage var helps us config where we wanna store our uploaded files
+            storage: diskStorage({  //diskStorage is our backend source code
+                destination: (req, file, cb) => {   //
+                    const folder = req?.headers?.folder_type ?? "default";
+                    this.ensureExists(`public/images/${folder}`);
+                    cb(null, join(this.getRootPath(), `public/images/${folder}`))
+                },
+                filename: (req, file, cb) => {  //helps us change our files names
+                    //get image extension
+                    let extName = path.extname(file.originalname);
+
+                    //get image's name (without extension)
+                    let baseName = path.basename(file.originalname, extName);
+
+                    let finalName = `${baseName}-${Date.now()}${extName}`
+                    cb(null, finalName)
+                }
+            })
+        };
+    }
 }
