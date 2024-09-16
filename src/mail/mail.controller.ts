6827ab7979937228d03dea192305590528840eed
Duc <thienductranhuu2784@gmail.com>
2024-09-16 00:50:29 -0400

    dynamically passing data into hbs file with loops

--- src/mail/mail.controller.ts
+++ src/mail/mail.controller.ts
@@ -14,13 +14,32 @@ export class MailController {
   @Public()
   @ResponseMessage("Test email")
   async handleTestEmail() {
+    const jobs = [
+      {name: "abc xyz job",
+      company: "ducc",
+      salary: "5000",
+      skills: ["React", "Node.js"]
+      },
+      {name: "abc xyz job2222",
+        company: "ducc",
+        salary: "5000",
+        skills: ["React", "Node.js"]
+      },
+      {name: "abc xyz job3333",
+        company: "ducc",
+        salary: "5000",
+        skills: ["React", "Node.js"]
+      }
+    ]
+
     await this.mailerService.sendMail({
       to: "thduc57@gmail.com",
       from: '"Support Team" <support@example.com>', // override default from
       subject: 'Welcome to Nice App! Confirm your Email',
       template: "new-job", // HTML body content
       context: {
-        receiver: "Duc"
+        receiver: "Duc",
+        jobs: jobs
       }
     });
   }
